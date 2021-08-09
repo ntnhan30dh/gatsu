52956468787d5b77c743049f4bec3853b09bfdfe
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2021-08-09 11:19:42 +0200

    add link to IG

--- src/components/followus.js
+++ src/components/followus.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 import MarqueeBlue from "./marquee_blue"
 import gatsu from "../images/gatsu_guy2.png"
 import Circle from "./circle"
-
+import { Link } from "gatsby"
 const Followus = props => {
   const [insta, setInsta] = useState(undefined)
   const token =
@@ -22,29 +22,36 @@ const Followus = props => {
   }, [])
   return (
     <section className={`followUsWrap  ${props.bg}`}>
+    <Link to="https://www.instagram.com/gatsugatsu_delivery" target="_blank" >
       <MarqueeBlue
         bg={props.bg==="bg-blue"?"bg-orange":"bg-blue"}
         text="* follow our shenanigans on instagram * "
       />
+      </Link>
       <div className="top w-full md:flex flex-row-reverse sm:p-10 md:mt-16 lg:px-30 xl:px-40 //xl:pt-28 //2xl:px-60 justify-center ">
+          <Link to="https://www.instagram.com/gatsugatsu_delivery" target="_blank" >
         <div className="pic relative -mb-20 sm:-mb-32 md:-mt-14 lg:-mt-24 xl:-mt-32 lg:-ml-20 xl:-ml-24  ml-1/2 sm:ml-8/12 md:ml-auto max-w-max ">
           <div className=" w-60 lg:w-80 xl:w-96 relative z-10">
             <img src={gatsu} alt="gatsu" />
           </div>
           <div className="absolute top-8 md:-top-2 lg:top-12 xl:top-16 -left-4 md:-left-20 lg:-left-16 xl:-left-10 ">
             <Circle color= {props.bg==="bg-blue"?"orange":"yellow"} text="ENJOY!" rotate="-rotate-12" />
-          </div>
         </div>
+          </div>
+            </Link>
         <div className="text -mt-10 ml-8 pb-10 sm:-mt-0 sm:ml-0 lg:pb-20 ">
+          <Link to="https://www.instagram.com/gatsugatsu_delivery" target="_blank" >
           <p
             className={`ginto ${
               props.bg === "bg-orange" ? "text-skin" : `text-skin`
-            } text-9vw //xsm:text-5xl //sm:text-4xl  md:text-6vw leading-100 //xl:text-7vw `}
+            } text-9vw   md:text-6vw leading-100 //xl:text-7vw `}
           >
+                  
             <div className="my-4 sm:my-8"> @ </div>
             GATSUGATSU _  <br />
             DELIVERY
           </p>
+                </Link>
         </div>
       </div>
 
